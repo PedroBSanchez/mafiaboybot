10cf344c2ccbe82d2712acc8ab915099d8952f24
PedroBSanchez <pedro_b47@hotmail.com>
2023-01-16 20:44:20 -0300

    Fix : deploy issues

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require("discord.js");
-const path = require("node:path");
-const fs = require("node:fs");
+const path = require("path");
+const fs = require("fs");
 
 const commands = [];
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require("fs");
+const path = require("path");
 dotenv.config();
 
 const {
